b087d45165b0ab347e60d02e91f3568e34da4738
hippalus <habiphakan.isler@gmail.com>
2023-10-27 02:32:41 +0200

    sort by title

--- src/main/kotlin/com/kramphub/api/SearchApiModels.kt
+++ src/main/kotlin/com/kramphub/api/SearchApiModels.kt
@@ -8,28 +8,28 @@ data class SearchResponse(
     val books: List<BookResponse>,
     val albums: List<AlbumResponse>
 ) {
-
+    
     companion object {
-
+        
         fun from(searchResult: SearchResult): SearchResponse = SearchResponse(
-            books = searchResult.books.map { BookResponse.from(it) },
-            albums = searchResult.albums.map { AlbumResponse.from(it) }
+            books = searchResult.books.map { BookResponse.from(it) }.sortedBy { it.title },
+            albums = searchResult.albums.map { AlbumResponse.from(it) }.sortedBy { it.title }
         )
     }
 }
 
 data class BookResponse(val title: String, val authors: List<String>?) {
-
+    
     companion object {
-
+        
         fun from(book: Book): BookResponse = BookResponse(book.title, book.authors)
     }
 }
 
 data class AlbumResponse(val title: String, val artist: String?) {
-
+    
     companion object {
-
+        
         fun from(album: Album): AlbumResponse = AlbumResponse(album.title, album.artist)
     }
 }
